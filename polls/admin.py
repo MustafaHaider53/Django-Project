006b0b8952099e132ce08c9cd3f1fa5baa88d8c9
MustafaHaider523 <mustafahaider523@gmail.com>
2025-06-25 11:05:57 +0500

    add Question class

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,13 @@ from .models import Question
 # Register your models here.
 
 
-admin.site.register(Question)
+from django.contrib import admin
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)

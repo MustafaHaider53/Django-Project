926d14ac4594269249847798ccae25f154645347
MustafaHaider523 <mustafahaider523@gmail.com>
2025-06-25 11:12:02 +0500

    add Tabular line in Choice

--- polls/admin.py
+++ polls/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
